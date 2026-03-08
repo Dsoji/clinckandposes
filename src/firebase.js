dec6fa24d02903c3cd072b7b18faf383a4f96d75
Oluwadamilola Olasoji <dami.osoj@gmail.com>
2026-03-08 19:14:28 +0100

    feat: Externalize Firebase configuration to environment variables and update .gitignore to ignore them.

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsRjxfP0xxP67b7Oj0G3Njio2rQXcHbFA",
-    authDomain: "clickandposes.firebaseapp.com",
-    projectId: "clickandposes",
-    storageBucket: "clickandposes.firebasestorage.app",
-    messagingSenderId: "578422181658",
-    appId: "1:578422181658:web:61e4b0b2e8601d28d8e6a3",
-    measurementId: "G-DJ0REJD5RW",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
